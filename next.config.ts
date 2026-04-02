ab21dfc18ab27422d7650bc6f896a9137dafd7ec
Ismail <115149488+ismail885@users.noreply.github.com>
2026-04-02 20:26:06 +0200

    Optimisation du chargement d'image dans next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
   images: {
     remotePatterns: [
       {
@@ -9,7 +12,12 @@ const nextConfig: NextConfig = {
         hostname: 'images.unsplash.com',
       },
     ],
+    formats: ['image/avif', 'image/webp'],
   },
+  experimental: {
+    optimizePackageImports: ['lucide-react', '@supabase/supabase-js'],
+  },
+  trailingSlash: false,
 };
 
 export default nextConfig;
